970b4a2e8c96b9365c3858e4398b07e430317733
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-11-02 02:41:36 +0700

    remove table

--- db/db.go
+++ db/db.go
@@ -12,7 +12,7 @@ var db *gorm.DB
 var err error
 
 func InitDB() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/sandbox_service_product?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(db_mysql)/sandbox_service_product?charset=utf8mb4&parseTime=True&loc=Local"
 	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
